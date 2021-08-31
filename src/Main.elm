a3c61aeb72a773045616ac80045ce77bab90abbe
Kasper Bruus Frank <kasperbfrank@gmail.com>
2021-08-31 12:59:21 +0200

    Chapter 8 (at section 8.2.3)

---
+++ src/Main.elm
@@ -0,0 +1,131 @@
+module Main exposing (main)
+
+import Browser exposing (Document)
+import Browser.Navigation as Nav
+import Html exposing (Html, a, footer, h1, li, nav, text, ul)
+import Html.Attributes exposing (classList, href)
+import Html.Lazy exposing (lazy)
+import Url exposing (Url)
+import Url.Parser as Parser exposing ((</>), Parser, s, string)
+
+
+type alias Model =
+    { page : Page }
+
+
+type Page
+    = Gallery
+    | SelectedPhoto String
+    | Folders
+    | NotFound
+
+
+view : Model -> Document Msg
+view model =
+    let
+        content =
+            text "This isn't even my final form!"
+    in
+    { title = "Photo Groove, SPA Style"
+    , body =
+        [ lazy viewHeader model.page
+        , content
+        , viewFooter
+        ]
+    }
+
+
+viewFooter : Html msg
+viewFooter =
+    footer [] [ text "One is never alone with a rubber duck. -Douglas Adams" ]
+
+
+viewHeader : Page -> Html Msg
+viewHeader page =
+    let
+        logo =
+            h1 [] [ text "Photo Groove" ]
+
+        links =
+            ul []
+                [ navLink Folders { url = "/", caption = "Folders" }
+                , navLink Gallery { url = "/", caption = "Gallery" }
+                ]
+
+        navLink : Page -> { url : String, caption : String } -> Html msg
+        navLink targetPage { url, caption } =
+            li [ classList [ ( "active", isActive { link = targetPage, page = page } ) ] ]
+                [ a [ href url ] [ text caption ] ]
+    in
+    nav [] [ logo, links ]
+
+
+isActive : { link : Page, page : Page } -> Bool
+isActive { link, page } =
+    case ( link, page ) of
+        ( Gallery, Gallery ) ->
+            True
+
+        ( Gallery, _ ) ->
+            False
+
+        ( Folders, Folders ) ->
+            True
+
+        ( Folders, SelectedPhoto _ ) ->
+            True
+
+        ( Folders, _ ) ->
+            False
+
+        ( SelectedPhoto _, _ ) ->
+            False
+
+        ( NotFound, _ ) ->
+            False
+
+
+type Msg
+    = NothingYet
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+supscriptions : Model -> Sub Msg
+supscriptions model =
+    Sub.none
+
+
+init : () -> Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( { page = urlToPage url }, Cmd.none )
+
+
+urlToPage : Url -> Page
+urlToPage url =
+    Parser.parse parser url
+        |> Maybe.withDefault NotFound
+
+
+parser : Parser (Page -> a) a
+parser =
+    Parser.oneOf
+        [ Parser.map Folders Parser.top
+        , Parser.map Gallery (s "gallery")
+        , Parser.map SelectedPhoto (s "photos" </> Parser.string)
+        ]
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , onUrlRequest = \_ -> Debug.todo "handle URL requests"
+        , onUrlChange = \_ -> Debug.todo "handle URL changes"
+        , subscriptions = supscriptions
+        , update = update
+        , view = view
+        }
